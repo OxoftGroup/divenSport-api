8bcd741b8825972976d1f411f562d6a4a1ffa36d
Juan Lunar <juanjlunarc@gmail.com>
2018-02-23 12:54:38 -0400

    Routes relocation
    Changing routes from web to api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('field', 'FieldController');
+
+Route::resource('game', 'GameController');
+
+Route::resource('geolocation', 'GeolocationController');
+
+Route::resource('notification', 'NotificationController');
+
+Route::resource('profile', 'ProfileController');
+
+Route::resource('rating', 'RatingController');
+
+Route::resource('sport', 'SportController');
+
+Route::resource('user', 'UserController');
